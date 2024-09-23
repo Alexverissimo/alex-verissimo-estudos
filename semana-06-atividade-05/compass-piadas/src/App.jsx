e3cd1004537ab2c4e54b10d8362c04d4ace72474
Alex verissimo <alexverissimo98@gmail.com>
2024-09-23 14:49:45 -0300

    feat: app  project add

---
+++ semana-06-atividade-05/compass-piadas/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Piadas from './Components/Piadas'
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Compass Piadas</h1>
+    <Piadas/>
+
+    </>
+  )
+}
+
+export default App

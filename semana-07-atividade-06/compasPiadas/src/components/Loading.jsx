3e27ebac2522fb36ce1c1921950e83aeba1fa9a7
Alex verissimo <alexverissimo98@gmail.com>
2024-09-30 17:15:55 -0300

    feat: loading initail add

---
+++ semana-07-atividade-06/compasPiadas/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="loading">
+      <img src="/src/assets/imgs/loading-load.gif" />
+      <p>carregando...</p>
+    </div>
+  );
+}
+
+export default Loading;
